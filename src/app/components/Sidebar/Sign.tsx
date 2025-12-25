8c143faf2bfea53357464260d15ae31f02ad0e89
Marhoosh <952128641@qq.com>
2025-12-25 21:07:34 +0800

    登录成功后不显示登录按钮

--- src/app/components/Sidebar/Sign.tsx
+++ src/app/components/Sidebar/Sign.tsx
@@ -1,6 +1,14 @@
 import { FC } from 'react'
+import { useSession } from '../session/SessionContext'
 
 const Sign: FC<{ text: string }> = ({ text }) => {
+  const { session } = useSession();
+  
+  // 如果用户已登录，不显示Sign in按钮
+  if (session) {
+    return null;
+  }
+
   return (
     <div className='rounded-[10px] w-full pl-3 flex flex-row gap-3 items-center shrink-0 py-[11px] justify-center'>
 
